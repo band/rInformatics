b03c8ae927c5166f06fe3aca0a3c5a7fae2d4787
William L. Anderson <band@acm.org>
2015-04-05 13:52:30 -0500

    reading repo node

--- re3data/xml-twig_readRepo.pl
+++ re3data/xml-twig_readRepo.pl
@@ -3,19 +3,20 @@
 use strict;
 use XML::Twig;
 
-my $file = 'repositories_0324.xml';
+my $file = 'r3dSampleRecord.xml';
 my $twig = XML::Twig->new();
 
 $twig->parsefile($file);
 
 my $root = $twig->root;
 
-print $root;
+print $root->first_child_text('repositoryName');
+print ' (' . $root->first_child_text('type') . ') ';
 print "\n";
 
-foreach my $repo ($root->children('repository')){
-    print $repo->first_child_text('id');
-    print ' (' . $repo->first_child_text('name') . ') ';
-    print $repo->first_child('link')->att('rel');
-    print "\n";
-}
+#foreach my $repo ($root->children('repository')){
+#    print $repo->first_child_text('id');
+#    print ' (' . $repo->first_child_text('name') . ') ';
+#    print $repo->first_child('link')->att('rel');
+#    print "\n";
+#}
