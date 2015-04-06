86b60fbc8077d0a52ac1b0f915033381d852a1e6
William L. Anderson <band@acm.org>
2015-04-06 15:11:34 -0500

    baseline: can read leaf content

---
+++ re3data/libxmlRead.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+# Script to illustrate how to parse a simple XML file
+# and dump its contents in a Perl hash record.
+
+use strict;
+use XML::LibXML;
+use Data::Dumper;
+
+# check for the correct number of command-line args
+if (($#ARGV + 1) != 1) {
+    print "\nUsage: xmlsimpleRead.pl repoRecordFile_name\n";
+    exit;
+}
+
+my $parser = XML::LibXML->new();
+my $doc    = $parser->parse_file($ARGV[0]);
+
+print $_->data . "\n" foreach ($doc->findnodes('//repository/contentType/text()'));
+
+
